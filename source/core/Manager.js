2c9d343e50820ebc9654e333301c9119907ca889
Renato Pereira <renato.ppontes@gmail.com>
2016-12-18 14:16:37 -0200

    Manager documentation

--- source/core/Manager.js
+++ source/core/Manager.js
@@ -1,19 +1,68 @@
-
+/**
+ * Base class for all managers in Skald.
+ *
+ * This class provides just the skeleton and base initialization for managers.
+ */
 export default class Manager {
   constructor() {
-    this.game = null
+    this._game = null
   }
 
+
+  /**
+   * The game instance. Readonly.
+   * @type {Game}
+   */
+  get game() {
+    return this._game
+  }
+
+
+  /**
+   * Setup method. Called internally by game, do not call manually.
+   *
+   * @param {Game} game - The game instance
+   */
   setup(game) {
-    this.game = game
+    this._game = game
   }
   
+  /**
+   * preUpdate method. Called internally by game, do not call manually.
+   *
+   * @param {Number} delta - Elapsed microseconds between this tick and the 
+   *        last one.
+   */
   preUpdate(delta) {}
+
+  /**
+   * Update method. Called internally by game, do not call manually.
+   *
+   * @param {Number} delta - Elapsed microseconds between this tick and the 
+   *        last one.
+   */
   update(delta) {}
+
+  /**
+   * postUpdate method. Called internally by game, do not call manually.
+   *
+   * @param {Number} delta - Elapsed microseconds between this tick and the 
+   *        last one.
+   */
   postUpdate(delta) {}
 
+  /**
+   * preDraw method. Called internally by game, do not call manually.
+   */
   preDraw() {}
+
+  /**
+   * draw method. Called internally by game, do not call manually.
+   */
   draw() {}
 
+  /**
+   * destroy method. Called internally by game, do not call manually.
+   */
   destroy() {}
 }
