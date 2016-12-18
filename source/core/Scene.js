26551137365f6eefbd79468dda464b4e773c034a
Renato Pereira <renato.ppontes@gmail.com>
2016-12-18 13:19:12 -0200

    Scene skeleton

--- source/core/Scene.js
+++ source/core/Scene.js
@@ -1,2 +1,21 @@
+import EventEmitter from 'core/EventEmitter'
 
-export default class Scene {}
+export default class Scene extends EventEmitter {
+  constructor() {
+    this._world = new PIXI.Container()
+  }
+
+  setup(game) {}
+
+  enter() {}
+  start() {}
+  stop() {}
+  leave() {}
+
+  preUpadte(delta) {}
+  update(delta) {}
+  postUpdate(delta) {}
+
+  preDraw() {}
+  draw() {}
+}
