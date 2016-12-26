fda88a68fdc3ec5f3ff1760c3c7654837e2451bb
Renato Pereira <renato.ppontes@gmail.com>
2016-12-26 15:28:02 -0200

    Keyboard manager docs

--- source/core/managers/KeyboardManager.js
+++ source/core/managers/KeyboardManager.js
@@ -2,8 +2,25 @@ import Manager from 'core/Manager'
 import KeyboardEvent from 'core/events/KeyboardEvent'
 
 /**
- * This manager handle the keyboard state. It is created by the game and can be
+ * A manager that handles the keyboard state. It is created by the game and can be
  * accessed via `game.keyboard`.
+ *
+ * This manager uses the browser events to keep the keyboard state updated. The
+ * state is the set of the status of each individual keys in a given time step.
+ *
+ * You may use the constants {@link KEYS} with this manager.
+ *
+ * Usage example:
+ *
+ *    update() {
+ *      if (game.keyboard.isDown(sk.KEYS.UP)) {
+ *        // move play forward
+ *      })
+ *
+ *      if (game.keyboard.isDown(sk.KEYS.SPACE)) {
+ *        // shoot!
+ *      }
+ *    }
  */
 export default class KeyboardManager extends Manager {
   
@@ -19,69 +36,50 @@ export default class KeyboardManager extends Manager {
     this._allowEvents     = null
   }
 
+  /**
+   * Whether this manager should prevent the default keyboard event behavior or
+   * not.
+   * @type {Boolean}
+   */
   get preventDefaults() { return this._preventDefaults }
   set preventDefaults(value) { this._preventDefaults = !!value}
 
+  /**
+   * Whether this manager should dispatch events or not.
+   * @type {Boolean}
+   */
   get allowEvents() { return this._allowEvents }
   set allowEvents(value) { this._allowEvents = !!value}
-
   
+  /**
+   * Setup the this manager. Called by the engine, do not call it manually.
+   */
   setup() {
     this._setupConfig()
     this._setupEvents()
   }
 
+  /**
+   * Post update method. Called by the engine, do not call it manually.
+   *
+   * @param {Number} delta - The elapsed time.
+   */
   postUpdate(delta) {
     this._lastState = this._state.slice()
   }
 
-  isDown(key) {
-    return this._state.indexOf(key) >= 0
-  }
-
-  isUp(key) {
-    return this._state.indexOf(key) < 0
-  }
-
-  isPressed(key) {
-    return this._state.indexOf(key) >= 0 &&
-           this._lastState.indexOf(key) < 0
-  }
-
-  isReleased(key) {
-    return this._state.indexOf(key) < 0 &&
-           this._lastState.indexOf(key) >= 0
-  }
-
-  isAnyDown(key) {
-    return !!this._state.length 
-  }
-
-  isAnyPressed(key) {
-    for (let i=0; i<this._state.length; i++) {
-      let key = this._state[i];
-      if (this._lastState.indexOf(key) < 0) return true
-    }
-  
-    return false
-  }
-
-  isAnyReleased(key) {
-    for (let i=0; i<this._lastState.length; i++) {
-      let key = this._lastState[i];
-      if (this._state.indexOf(key) < 0) return true
-    }
-
-    return false
-  }
-
-
+  /**
+   * Setup the manager variables using the game configuration.
+   */
   _setupConfig() {
     let config = this.game.config
     this._preventDefaults = config.keyboard.preventDefaults
     this._allowEvents     = config.keyboard.allowEvents
   }
 
+  /**
+   * Setup the events on the canvas.
+   */
   _setupEvents() {
     let view = this.game.renderer.view
     view.addEventListener('blur', e=>this._onBlur(e), false)
@@ -91,11 +89,17 @@ export default class KeyboardManager extends Manager {
     view.addEventListener('keyup', e=>this._onKeyUp(e), false)
   }
 
-  _dispatchEvent(type, event) {
+  /**
+   * Dispatch a browser keyboard event to the game.
+   *
+   * @param {String} eventType - The type of the event.
+   * @param {Event} event - The browser event.
+   */
+  _dispatchEvent(eventType, event) {
     if (!this._allowEvents) return
 
     this.game.events.dispatch(new KeyboardEvent(
-      type,
+      eventType,
       event.keyCode||event.which,
       event.shiftKey,
       event.ctrlKey,
@@ -105,11 +109,28 @@ export default class KeyboardManager extends Manager {
     ))
   }
 
+  /**
+   * Callback for the canvas blur event.
+   *
+   * @param {Event} event - The browser event.
+   */
   _onBlur(event) {
     this._state = []
   }
+
+  /**
+   * Callback for the canvas focus event.
+   *
+   * @param {Event} event - The browser event.
+   */
   _onFocus(event) {
   }
+
+  /**
+   * Callback for the canvas keydown event.
+   *
+   * @param {Event} event - The browser event.
+   */
   _onKeyDown(event) {
     // Pressing key the first time
     if (!event.repeat) {
@@ -126,12 +147,24 @@ export default class KeyboardManager extends Manager {
       return false
     }
   }
+
+  /**
+   * Callback for the canvas keypress event.
+   *
+   * @param {Event} event - The browser event.
+   */
   _onKeyPress(event) {
     if (this._preventDefaults) {
       event.preventDefault()
       return false
     }
   }
+
+  /**
+   * Callback for the canvas keyup event.
+   *
+   * @param {Event} event - The browser event.
+   */
   _onKeyUp(event) {
     this._state.splice(this._state.indexOf(event.keyCode||event.which), 1)
     this._dispatchEvent('keyup', event)
@@ -141,4 +174,91 @@ export default class KeyboardManager extends Manager {
       return false
     }
   }
+
+  /**
+   * Verifies if a given key is down.
+   *
+   * @param {Number} key - The key code.
+   * @return {Boolean} The key status.
+   */
+  isDown(key) {
+    return this._state.indexOf(key) >= 0
+  }
+
+  /**
+   * Verifies if a given key is up.
+   *
+   * @param {Number} key - The key code.
+   * @return {Boolean} The key status.
+   */
+  isUp(key) {
+    return this._state.indexOf(key) < 0
+  }
+
+  /**
+   * Verifies if a given key has changed its status from up to down (this is
+   * only valid for a single tick).
+   *
+   * @param {Number} key - The key code.
+   * @return {Boolean} The key status.
+   */
+  isPressed(key) {
+    return this._state.indexOf(key) >= 0 &&
+           this._lastState.indexOf(key) < 0
+  }
+
+  /**
+   * Verifies if a given key has changed its status from down to up (this is
+   * only valid for a single tick).
+   *
+   * @param {Number} key - The key code.
+   * @return {Boolean} The key status.
+   */
+  isReleased(key) {
+    return this._state.indexOf(key) < 0 &&
+           this._lastState.indexOf(key) >= 0
+  }
+
+  /**
+   * Verifies if any key is down.
+   *
+   * @param {Number} key - The key code.
+   * @return {Boolean} The key status.
+   */
+  isAnyDown(key) {
+    return !!this._state.length 
+  }
+
+  /**
+   * Verifies if any key has changed its status from up to down (this is only
+   * valid for a single tick).
+   *
+   * @param {Number} key - The key code.
+   * @return {Boolean} The key status.
+   */
+  isAnyPressed(key) {
+    for (let i=0; i<this._state.length; i++) {
+      let key = this._state[i];
+      if (this._lastState.indexOf(key) < 0) return true
+    }
+  
+    return false
+  }
+
+  /**
+   * Verifies if any key has changed its status from down to up (this is only
+   * valid for a single tick).
+   *
+   * @param {Number} key - The key code.
+   * @return {Boolean} The key status.
+   */
+  isAnyReleased(key) {
+    for (let i=0; i<this._lastState.length; i++) {
+      let key = this._lastState[i];
+      if (this._state.indexOf(key) < 0) return true
+    }
+
+    return false
+  }
+
 }
