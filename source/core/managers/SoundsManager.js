69b41c61af9687c32406cbf736857083395d1cd4
Renato Pereira <renato.ppontes@gmail.com>
2017-01-05 19:48:42 -0200

    Docs

--- source/core/managers/SoundsManager.js
+++ source/core/managers/SoundsManager.js
@@ -2,20 +2,36 @@ import Manager from 'core/Manager'
 import * as audio from 'audio'
 
 /**
- * - https://www.html5rocks.com/en/tutorials/webaudio/intro/
+ * The sounds manager handle the storage and initialization of the game audios.
+ * This manager is created by the game and can be accessed via `game.sounds`.
  *
- * 
- * - https://github.com/goldfire/howler.js
- * - https://github.com/danrossi/audio-engine
- * - https://github.com/johnhornsby/audioengine
- * - https://github.com/cykod/Quintus
- * - https://github.com/CreateJS/SoundJS
- * - https://github.com/photonstorm/phaser/tree/master/v2/src/sound
- * - https://github.com/kittykatattack/sound.js
- * - https://github.com/jaysalvat/buzz
- * - http://zohararad.github.io/audio5js/
- * - https://galactic.ink/midi-js/
+ * Skald implements a flexible audio system, which currently accepts WebAudio
+ * and HTML5 audio, but can be expanded to more systems. Check it out the
+ * {@link AudioSystem} class and the `skald.audio` package to learn more and
+ * see implementation examples.
  *
+ * Our audio system is based on [Howler](https://github.com/goldfire/howler.js),
+ * in which each audio file is represented by an Audio object, and it can be
+ * played multiple times (or not, depending on your configuration) with 
+ * different settings. Check the {@link BaseAudio} interface to see more about
+ * what you can do with the audio objects.
+ *
+ * Right now, we only support loading sounds via `resources` manager.
+ *
+ * You may set which audio systems may work and in which order they will be 
+ * tested in your game:
+ *
+ *     skald.globals.setAudioSystems([
+ *       skald.audio.WebAudioSystem,
+ *       skald.audio.HTML5System,
+ *       MyCustomSystem
+ *     ])
+ *     game = new skald.Game()
+ *
+ * The manager will test each system to verify if it can run in the user 
+ * platform, and will select the first one that works. Notice that, you must
+ * set the audio systems BEFORE the creation of the game and it can't be 
+ * changed after that.
  *
  *
  * Role:
@@ -24,18 +40,7 @@ import * as audio from 'audio'
  * - Handle sound groups
  * - Initialize and configure the browser audio system
  * - Store individual audios
- *
- * AudioSystem:
- *
- * - Initialize specific audio implementation (webaudio, html5, flash, ...)
- * - Create audio objects from sources
- *
- * Audio:
- *
- * - Object representing an audio source
- * - Play, pause, stop, resume
- * - May play more than one sound
- * 
+ * * 
  */
 export default class SoundsManager extends Manager {
   
@@ -47,6 +52,9 @@ export default class SoundsManager extends Manager {
   }
 
   get system() { return this._system }
+
+  get volume() {}
+  set volume(v) {}
   
   setup() {
     this._system = new audio.WebAudioSystem()
