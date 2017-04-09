c55e0efd7159b340c0d74040fbf83a38c151470e
Renato Pereira <renato.ppontes@gmail.com>
2017-04-09 02:27:45 -0300

    utils package

---
+++ source/utils/index.js
@@ -0,0 +1,7 @@
+// import * as logging from 'utils/logging'
+import * as easing from 'utils/easing'
+// import * as profiling from 'utils/profiling'
+
+export {easing}
+// export {logging, easing, profiling}
+// export * from 'utils/functions'
