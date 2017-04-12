b2bb8849dff6018d13bf50614116e02a3fa8ea80
Renato Pereira <renato.ppontes@gmail.com>
2017-04-12 19:03:28 -0300

    Structure and events for the resources manager

--- source/events_/index.js
+++ source/events_/index.js
@@ -1,2 +1,5 @@
 export { default as Event } from 'events_/Event'
+export { default as ErrorEvent } from 'events_/ErrorEvent'
+export { default as ProgressEvent } from 'events_/ProgressEvent'
+export { default as ResourceEvent } from 'events_/ResourceEvent'
 export { default as OrientationEvent } from 'events_/OrientationEvent'
