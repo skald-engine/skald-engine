2011039cafff5c8c26b661f777c3cfe8d4444dac
Renato Pereira <renato.ppontes@gmail.com>
2017-05-01 09:28:51 -0300

    util functions in the correct directory

--- source/sk/utils/functions/index.js
+++
@@ -1,7 +0,0 @@
-export {default as enumeration} from 'sk/utils/functions/enumeration.js'
-export {default as validateJson} from 'sk/utils/functions/validateJson.js'
-export {default as deepCopy} from 'sk/utils/functions/deepCopy.js'
-export {default as deepMerge} from 'sk/utils/functions/deepMerge.js'
-export {default as clip} from 'sk/utils/functions/clip.js'
-export {default as isFunction} from 'sk/utils/functions/isFunction.js'
-export {default as createClass} from 'sk/utils/functions/createClass.js'
--- source/sk/utils/index.js
+++ source/sk/utils/index.js
@@ -1,6 +1,12 @@
 import * as logging from 'sk/utils/logging'
 import * as easing from 'sk/utils/easing'
 import * as profiling from 'sk/utils/profiling'
-
 export {logging, easing, profiling}
-export * from 'sk/utils/functions'
+
+export {default as enumeration} from 'sk/utils/enumeration.js'
+export {default as validateJson} from 'sk/utils/validateJson.js'
+export {default as deepCopy} from 'sk/utils/deepCopy.js'
+export {default as deepMerge} from 'sk/utils/deepMerge.js'
+export {default as clip} from 'sk/utils/clip.js'
+export {default as isFunction} from 'sk/utils/isFunction.js'
+export {default as createClass} from 'sk/utils/createClass.js'
