6943bd820147228babbccba3e1a16749347d5b10
Renato Pereira <renato.ppontes@gmail.com>
2017-05-10 18:01:47 -0300

    Fixing wrong files

--- source/sk/utils/random/inclusiveRandom.js
+++ source/sk/utils/random/inclusiveRandom.js
@@ -1,11 +1,18 @@
-describe('sk/utils/random/inclusiveRandom.js', () => {
-  let inclusiveRandom = require('./inclusiveRandom.js').default
+/**
+ * Generates a random number between 0 (inclusive) and 1 (inclusive).
+ *
+ * This function uses rejection sampling to guarantee that it is possible to 
+ * return a number in the correct interval. Internally it generates a number 
+ * in [-.1, 1.1), but rejects any number that is not contained in the interval
+ * [0, 1].
+ *
+ * @return {Number}
+ */
+export default function inclusiveRandom() {
+  let n = -1
+  while (n < 0 || n > 1) {
+    n = Math.random()*1.2 - 0.1
+  }
 
-  it('should generate a random number between 0 and 1, both inclusive', () => {
-    for (let i=0; i<5000; i++) {
-      let number = inclusiveRandom()
-      assert.isTrue(number >= 0, `Error "${number}" should be greater or equal to 0.`)
-      assert.isTrue(number <= 1, `Error "${number}" should be lesser or equal to 1.`)
-    }
-  })
-})
+  return n
+}
