41eb352e481995a3e195a1095582249f8f13d50b
Renato Pereira <renato.ppontes@gmail.com>
2017-05-10 18:44:14 -0300

    Adding choose to the package

--- source/sk/utils/random/index.js
+++ source/sk/utils/random/index.js
@@ -1,3 +1,4 @@
 export {default as int} from 'sk/utils/random/int'
 export {default as polar} from 'sk/utils/random/polar'
 export {default as inclusiveRandom} from 'sk/utils/random/inclusiveRandom'
+export {default as choose} from 'sk/utils/random/choose'
