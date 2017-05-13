9520fea3bb3dbd85c37dc2e925b9bcca06c9e7ef
Renato Pereira <renato.ppontes@gmail.com>
2017-05-13 20:04:54 -0300

    Adding uniform option, and using radius instead of diameter

--- source/sk/particles/emissors/RingEmissor.js
+++ source/sk/particles/emissors/RingEmissor.js
@@ -7,32 +7,33 @@ import * as utils from 'sk/utils'
  * This emissor will generate points a ring area, in which:
  *
  * - CENTER AT: [emitter.emissionX, emitter.emissionY]
- * - DIAMETER: emissor.diameter
+ * - RADIUS: emissor.radius
  * - WIDTH: emissor.width
  *
- * The new points will generate a point from `diameter/2-width`px from the 
- * center to `diameter/2`px from the center.
+ * The new points will generate a point from `radius-width`px from the 
+ * center to `radius`px from the center.
  */
 export default class RingEmissor extends Emissor {
   /**
    * Constructor.
    *
-   * @param {Number} [diameter=100] - Diamater of the ring.
+   * @param {Number} [radius=50] - Radius of the ring.
    * @param {Number} [width=30] - The width of the ring.
    */
-  constructor(diameter=100, width=30) {
+  constructor(radius=50, width=30) {
     super()
 
-    this._diameter = diameter
+    this._radius = radius
     this._width = width
+    this._uniform = false
   }
 
   /**
    * Diameter of the ring.
    * @type {Number}
    */
-  get diameter() { return this._diameter }
-  set diameter(v) { this._diameter = v }
+  get radius() { return this._radius }
+  set radius(v) { this._radius = v }
 
   /**
    * Width of the ring.
@@ -42,15 +43,33 @@ export default class RingEmissor extends Emissor {
   set width(v) { this._width = v }
 
   /**
+   * If true, the emissor will generate the points uniformly on the center, 
+   * avoiding the concentration of point in the center. DEfault to false.
+   *
+   * See http://www.anderswallin.net/2009/05/uniform-random-points-in-a-circle-using-polar-coordinates/ for an 
+   * explanation.
+   * 
+   * @type {Boolean}
+   */
+  get uniform() { return this._uniform }
+  set uniform(v) { this._uniform = !!v }
+
+  /**
    * Generate the points.
    */
   next() {
-    let r = Math.random()*Math.PI*2
-    let range = Math.random()*this._width + this._diameter/2 - this._width
+    let angle = Math.random()*Math.PI*2
+
+    let range = Math.random()
+    if (this._uniform) { // convert to uniform
+      range = Math.sqrt(range)
+    }
+    range = range*this._width + this._radius - this._width
+
 
     return {
-      x: this._emitter.emissionX + Math.sin(r)*range,
-      y: this._emitter.emissionY + Math.cos(r)*range
+      x: this._emitter.emissionX + Math.sin(angle)*range,
+      y: this._emitter.emissionY + Math.cos(angle)*range
     }
   }
 }
