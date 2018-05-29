e5eaadd8a33243803f90ea7af200541e4d38156c
Renato Pereira <renato.ppontes@gmail.com>
2018-05-29 00:39:01 -0300

    Fixing names on linux

--- src/sk/services/ConfigService.js
+++ src/sk/services/ConfigService.js
@@ -23,7 +23,9 @@ class ConfigService extends Service {
 
   get(name, default_=null) {
     let value = this._config[name]
-    return (typeof value === 'undefined' || value === null)? default_ : value
+    return (typeof value === 'undefined' || value === null)
+              ? default_
+              : value
   }
 }
 
